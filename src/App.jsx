15ec5de2fd92beba58db0976d3b1476fd4734d85
RinaVT <rinarin.rnd@yandex.ru>
2023-11-23 19:46:53 +0300

    new add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Card from "./components/Card";
 
+import data from "./assets/data.json";
+
 class App extends React.Component {
     render() {
         return (
             <div className="wrapper">
                 <div className="cards-container">
-                    <Card/>
+                    {/* <Card/> */}
+                    {data.map((d, i) => <Card 
+                        key={i}
+                        img={d.picture}
+                        text={d.name}
+                        price={d.price}
+                    />)}
                 </div>
             </div>
         )
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import "./style.css";
 
-const Card = () => {
+const Card = ({text, price, img}) => {
+    const imgStyle = {
+        backgroundImage: `url(${img})`
+    };
     return (
         <div className="card">
-            <div className="card_img"></div>
-            <div className="card_price">300 ₽</div>
-            <div className="card_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+            <div className="card_img" style={imgStyle}></div>
+            <div className="card_price">{price} ₽</div>
+            <div className="card_text">{text}</div>
             <button className="btn">В корзину</button>
         </div>
     )

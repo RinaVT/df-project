4e50298232ea1c01b313a6059b1ed594437370e7
RinaVT <136070386+RinaVT@users.noreply.github.com>
2023-12-10 22:47:11 +0300

    New add

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Logo from "../logo";
 import "./style.css";
 import {ReactComponent as FavIcon} from "./img/like_heart.svg";
@@ -7,12 +7,17 @@ import {ReactComponent as ProIcon} from "./img/dogFood_icon.svg";
 
 
 export default () => {
+    const [text, changeText] = useState();
+    let cnt = 0;
+    const handler = e => {
+        changeText(e.target.value);
+    }
     return <>
         <header>
             <div className="logo">
                 <Logo/>
             </div>
-            <input type="search"/>
+            <input type="search" value={text} onChange={handler}/>
             <nav>
                 <a href=""><FavIcon/></a>
                 <a href=""><CartIcon/></a>
@@ -20,7 +25,7 @@ export default () => {
             </nav>
         </header>
         <div>
-            Поиск...
+            {text ? `По запросу ${text} найдено ${cnt} позиций` : "Поиск..."}
         </div>
     </>
 }

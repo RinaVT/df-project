8325fb503b36d1edf57b6dfff6c2c634788652da
RinaVT <rinarin.rnd@yandex.ru>
2024-01-26 15:51:05 +0300

    New footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import Logo from "../logo";
 import "./style.css";
+import {ReactComponent as TelIcon} from "./icon/telegram.svg";
+import {ReactComponent as WatsIcon} from "./icon/watsup.svg";
+import {ReactComponent as InstIcon} from "./icon/instagram.svg";
+import {ReactComponent as VkIcon} from "./icon/vk.svg";
 
 const Footer = () => {
     return (
-        <div>
+        <footer>
             <div className="logoFooter">
                 <div className="logo">
                     <Logo/>
                 </div>
-                <p>Terms and Conditions • Privacy Policy • Cookie Policy</p>
+                <p className="pLogo">Terms and Conditions • Privacy Policy • Cookie Policy</p>
             </div>
-            <nav className="navigator">
-                <div>
-                    <a href="">Каталог</a>
-                    <a href="">Акции</a>
-                    <a href="">Новости</a>
-                    <a href="">Отзывы</a>
+            <div className="navigator">
+                <a href="">Каталог</a>
+                <a href="">Акции</a>
+                <a href="">Новости</a>
+                <a href="">Отзывы</a>
+            </div>
+            <div className="navigator">
+                <a href="">Оплата и доставка</a>
+                <a href="">Часто спрашивают</a>
+                <a href="">Обратная связь</a>
+                <a href="">Контакты</a>
+            </div>
+            <nav className="contacts">
+                <h3>Мы на связи</h3>
+                <div className="mailTel">
+                    <a className="tel" href="tel:+79990000000">8 (999) 000 - 00 - 00</a>
+                    <a className="mail" href="mailto:dogfood_ru@gmail.com">dogfood_ru@gmail.com</a>
                 </div>
-                <div>
-                    <a href="">Оплата и доставка</a>
-                    <a href="">Часто спрашивают</a>
-                    <a href="">Обратная связь</a>
-                    <a href="">Контакты</a>
+                <div className="contactsIcon">
+                    <a href=""><TelIcon/></a>
+                    <a href=""><WatsIcon/></a>
+                    <a href=""><InstIcon/></a>
+                    <a href=""><VkIcon/></a>
                 </div>
             </nav>
-            
-        </div>
+        </footer>
     )
 }
 
